0cae3e41c338eeff26ecf7e3172cbaf8132575ca
Abhay <abhay@Abhays-MacBook-Pro.local>
2026-04-15 17:48:26 +0530

    deploy update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,4 @@
-import { CodeXml, GraduationCap, Heart, Rocket } from "lucide-react";
+import { Bug, CodeXml, GraduationCap, Rocket } from "lucide-react";
 import React from "react";
 
 const About = () => {
@@ -67,8 +67,8 @@ const About = () => {
 
           {/* Card 4 */}
           <div className="bg-gray-800/40 backdrop-blur-md rounded-2xl border border-gray-700 flex flex-col items-center justify-center gap-2 p-4 sm:p-5 hover:scale-105 transition-all duration-300 shadow-md">
-            <Heart className="text-blue-500 w-6 h-6 sm:w-7 sm:h-7" />
-            <p className="text-white text-sm sm:text-base">Open Source</p>
+            <Bug className="text-blue-500 w-6 h-6 sm:w-7 sm:h-7" />
+            <p className="text-white text-sm sm:text-base">Problem Solving</p>
             <p className="text-gray-400 text-xs sm:text-sm text-center">
               Active Contributor
             </p>
